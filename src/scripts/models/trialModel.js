7707a93350cd4149159155c0cdfec95905368196
Caitlin Flattery <caitlinflattery@gmail.com>
2017-01-07 12:12:11 -0600

    added properties to models and collections

--- src/scripts/models/trialModel.js
+++ src/scripts/models/trialModel.js
@@ -1,9 +1,11 @@
 import Backbone from 'backbone'
 
 export const TrialModel = Backbone.Model.extend({
-    url: 'https://clinicaltrialsapi.cancer.gov/v1/clinical-trials'
+    url: 'https://clinicaltrialsapi.cancer.gov/v1/clinical-trials',
+    idAttribute: 'id'
 })
 
 export const TrialCollection = Backbone.Collection.extend({
-    url: 'https://clinicaltrialsapi.cancer.gov/v1/clinical-trials'
+    url: 'https://clinicaltrialsapi.cancer.gov/v1/clinical-trials',
+    model: TrialModel
 })
