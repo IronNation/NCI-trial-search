c82c66a53361f31bcb03c09f71200f2b50f32374
Caitlin Flattery <caitlinflattery@gmail.com>
2017-01-08 14:46:25 -0600

    commented out not work module

--- src/scripts/views/HomeView.js
+++ src/scripts/views/HomeView.js
@@ -8,22 +8,22 @@ class HomeView extends React.Component {
         return (
             <div className = 'homeView'>
                 <Header />
-                <Home />
+                {/*<Home />*/}
                 <ResultsList />
             </div>
         )
     }
 }
 
-class Home extends React.Component {
+// class Home extends React.Component {
 
-    render() {
-        return (
-            <div className = 'home'>
-                <h1>Search for clinical drug trials near you!</h1>
-            </div>
-        )
-    }
-}
+//     render() {
+//         return (
+//             <div className = 'home'>
+//                 <h1>Search for clinical drug trials near you!</h1>
+//             </div>
+//         )
+//     }
+// }
 
 export default HomeView
